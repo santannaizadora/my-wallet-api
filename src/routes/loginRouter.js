9edce048b7ce1b735cd80cdcd71cd0eb88998487
ElderGr <gbielrs@gmail.com>
2022-05-13 17:13:41 -0300

    Preparando revisão de código

--- src/routes/loginRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import loginUser from "../controllers/loginController.js";
-import validateLogin from "../middlewares/loginMiddleware.js";
-
-const registerRouter = Router();
-registerRouter.post("/login", validateLogin,loginUser);
-
-export default registerRouter;
--- src/routes/registerRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import registerUser from "./../controllers/registerController.js";
-import validateRegister from './../middlewares/registerMiddleware.js';
-
-const registerRouter = Router();
-registerRouter.post("/register",validateRegister, registerUser);
-
-export default registerRouter;
